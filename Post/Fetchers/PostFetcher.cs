001678c051264e6575a7e2313c2f0527c764b0cf
YumengLiu <cindyliu2016@gmail.com>
2020-11-29 17:07:15 +0100

    First check in

---
+++ Post/Fetchers/PostFetcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Post.Fetchers
+{
+    public class PostFetcher
+    {
+        private readonly HttpClient httpClient;
+        public PostFetcher()
+        {
+            httpClient = new HttpClient();
+        }
+
+        public async Task<string> FetchPosts()
+        {
+            Task<string> response = httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/posts");
+            string jsonString = await response;
+            return jsonString;
+        }
+    }
+}
---
+++ Post/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Post.Fetchers;
+
+namespace Post
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            PostFetcher postFetcher = new PostFetcher();
+            Console.WriteLine(await postFetcher.FetchPosts());
+        }
+    }
+}
